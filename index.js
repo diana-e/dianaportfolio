c16f370c4801bb9d4ba87e9b36012cabd0a4d65f
diana-e <dlellis2020@gmail.com>
2017-07-20 15:54:50 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+$("#filler").click(function(){
+    $("#filler").html("dropdowns are lame sidebuttons are better p.s. the dropdowns wouldnt actually link stuff :/"); });
+
+$("#Hackathons").click(function()
+    { $("#foot").append("https://diana-e.github.io/dianaportfolio/HackOne.html","https://diana-e.github.io/dianaportfolio/HackTwo.html");
+});
