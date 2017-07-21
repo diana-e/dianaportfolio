2678b07195e217a862236819c66e36f925f634b9
diana-e <dlellis2020@gmail.com>
2017-07-21 11:07:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $("#filler").click(function(){
-    $("#filler").html("dropdowns are lame sidebuttons are better p.s. the dropdowns wouldnt actually link stuff :/"); });
+    $("#filler").html(" p.s. the dropdowns wouldnt actually link stuff :/"); });
 
 $("#Hackathons").click(function()
     { $("#foot").append("https://diana-e.github.io/dianaportfolio/HackOne.html","https://diana-e.github.io/dianaportfolio/HackTwo.html");
